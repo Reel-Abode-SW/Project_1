87bba70cdff58eaafde28184f63d9c7166aaad63
Nicholas (Cpaz) <nicky.shadow99@gmail.com>
2016-07-16 17:04:19 -0400

    Crops lined up

--- farm_sim.py
+++ farm_sim.py
@@ -12,15 +12,18 @@ def main():
     mun=0; #money
     actions_per_day=5; #testing this thing
     crop_watered=False;
+    plot_click=0;
     plot_count=3; #0-based number of plots (so 4 plots)
+    i=0;
+    plot=[0,0,0,0];
     
     #Window setup
     win = GraphWin("Farm", 1280, 800);
     
     #Creating and drawing the boxes. (0,0) is in the top left, not bottom left
-    next = Text(Point(1200, 50), "Next Day");
+    next = Text(Point(1180, 50), "Next Day");
     next.setSize(30);
-    next_box = Rectangle(Point(1116, 28), Point(1285, 72));
+    next_box = Rectangle(Point(1096, 28), Point(1285, 72));
     next.draw(win); #Draw
     next_box.draw(win);
     
@@ -30,31 +33,35 @@ def main():
     exit.draw(win); #Draw
     exit_box.draw(win);
     
-    i=0;
-    plot=[0,0,0,0];
-    while i<plot_count:
-        plot[i] = Rectangle(Point(40+i, 40+(i)), Point(120+(i), 120+(i)));
+    while i<=plot_count:
+        plot[i] = Rectangle(Point(40+(90*i), 40), Point(120+(90*i), 120));
         #Center letter used to indicate what is being grown
         plot_type = Text(plot[i].getCenter(), "p");
         plot[i].draw(win);
         plot_type.draw(win);
+        plot[i].setFill("white");
         i+=1;
     
     end = False;
     while end == False:
         click = win.getMouse();
-        plot_click=(in_box(Point(40, 40), Point(120, 120), click));
-        
-        #End program if the exit box is clicked on
-        if in_box(Point(15, 745), Point(85, 785), click) == True:
-            end = True;
+        while i<=plot_count:
+            #plot_clock will point to the index in the list of plots
+            if (in_box(Point(40+(90*i), 40), Point(120+(90*i), 120), click)):
+                plot_click=i;
+                crop_watered=True;
+            i+=1;
         
         #Change box color to show that the crop has been watered
-        if crop_watered == True:
-            plot[0].setFill("sky blue");
         if crop_watered == False:
-            plot[0].setFill("white");
-            
+            plot[plot_click].setFill("white");
+        if crop_watered == True:
+            plot[plot_click].setFill("sky blue");
+        
+        #End program if the exit box is clicked on
+        if in_box(Point(0, 743), Point(87, 787), click) == True:
+            end = True;
+        
     win.close()
     
 main()
